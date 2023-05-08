3a8cfe87b2ddc32c24f32f0dc78d09edb25a06e2
aashem <shemeikka.asla@gmail.com>
2023-05-08 19:54:17 +0300

    lisätty toggleable näppäin

--- unikalenteri/src/App.js
+++ unikalenteri/src/App.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
-function Lisää_tapahtuma(){
-  alert("TÄHÄN SE RUUTU MISSÄ VOI LISÄTÄ TAPAHTUMAN")
-}
+
+
 
 
 function App() {
+  
+  /*
+  Pää funktio App()
+  */
+
+  //tilakoneet true, false
+  const [popup, setPopup] = useState(false)
+  const [painettu, setPainettu] = useState(true)
+
+  //funtkio joka ohjaa näppäimen toimintaa
+  const lisäyshandler = (e) => {
+    // muuttaa popupista true tai false jos se on true tai false, toglettaa onko visible vai hidden rivillä 35
+   popup ? setPopup(false) : setPopup(true)
+  }
   return (
     <div>
         <header className="Otsikko">UNIKALENTERI</header>
       <div className = "Pääruutu"><h1>Pääruutu</h1>
+      <div className = "Lisäysikkuna" style = {{visibility: popup ? "visible" : "hidden"}}>POPUP</div>
         <div className = "Kalenteri"><h1>Kalenteri</h1></div>
         <div className = "Infopaneeli">
           <h1>
@@ -17,7 +32,7 @@ function App() {
           </h1>
         </div>
         <div>
-          <button className = "Lisää" onClick={Lisää_tapahtuma}>
+          <button className = "Lisää" onClick={lisäyshandler} style ={{visibility: painettu ? "visible" : "hidden"}}>
             Lisää
           </button>
         </div>
