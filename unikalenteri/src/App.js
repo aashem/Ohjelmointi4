f36dee5880fa25e2258a27498bfc56e1dbe0a125
aashem <shemeikka.asla@gmail.com>
2023-05-11 21:38:00 +0300

    Tapahtuman lisäys näkyy infopaneelissa

--- unikalenteri/src/App.js
+++ unikalenteri/src/App.js
@@ -8,6 +8,7 @@ import suru from "./kuvat/Suru.jpg"
 
 function App() {
   //Tilat
+  const ids = ["hymy","midi","suru"]
   const [popup, setPopup] = useState(false)
   const [painettu, setPainettu] = useState(true)
   const [tapahtuma, setTapahtuma] = useState("")
@@ -16,9 +17,15 @@ function App() {
   const [tyyppi, setTyyppi] = useState("")
   const [kelloAlku, setKelloAlku] = useState("")
   const [kelloLoppu, setKelloLoppu] = useState("")
+  const [naama, setNaama] = useState(0)
+  const [muistiinpanot, setMuistiinpanot] = useState("")
+  const [nykyinenTapahtuma, setNykyinenTapahtuma] = useState("")
+  const [nykyinenKelloAlku, setNykyinenKelloAlku] = useState("")
+  const [nykyinenKelloLoppu, setNykyinenKelloLoppu] = useState("")
+  const [nykyinenNaama, setNykyinenNaama] = useState("")
+  const [nykyinenLaji, setNykyinenLaji] = useState("")
+  const [nykyinenMuistiinpanot, setNykyinenMuistiinpanot] = useState("")
 
-  //funtkio joka ohjaa näppäimen toimintaa
-  //Toglettaa visibility "visible" tai "hidden"
   const lisäyshandler = (e) => {
     popup ? setPopup(false) : setPopup(true)
   }
@@ -42,6 +49,28 @@ function App() {
   const kellohändlerloppu = (e) => {
     setKelloLoppu(e.target.value)
   }
+  const kuvahändler = (e) => {
+    setNaama(e.target.id)
+    let lista = ids 
+    for (let i = 0; i < lista.length; i++){
+      document.getElementById(lista[i]).setAttribute('style', 'border-style: dotted')    
+    }
+    document.getElementById(e.target.id).setAttribute('style', 'border-style: solid; border-radius: 25px; border-color: green')    
+  }
+  const muistiinpanohändler = (e) => {
+    setMuistiinpanot(e.target.value)
+  }
+
+  const lisäämerkintäkalenteriin = (e) => {
+    setNykyinenTapahtuma(tapahtuma)
+    setNykyinenLaji(laji)
+    setNykyinenKelloAlku(kelloAlku)
+    setNykyinenKelloLoppu(kelloLoppu)
+    setNykyinenNaama(naama)
+    setNykyinenMuistiinpanot(muistiinpanot)
+  }
+
+
 
   return (
     <div>
@@ -55,11 +84,12 @@ function App() {
           <h2>
             Tapahtuma:
           </h2>
-          <select className = "Valintaboxi" onChange={valintaboxihandler}>
+          <select className = "Valintaboxi" id="Valintaboxi" onChange={valintaboxihandler}>
+            <option value="" selected disabled hidden>Valitse tästä</option>
             <option value="Urheilu">Urheilu</option>
             <option value="Uni">Uni</option>
           </select>  
-          <div className= "Lajikenttä" style ={{visibility: lajiNäkyvyys && popup ? "visible" : "hidden"}}>
+          <div className= "Lajikenttä" id ="laji" style ={{visibility: lajiNäkyvyys && popup ? "visible" : "hidden"}}>
             <h2>
               Tyyppi:
             </h2>
@@ -71,20 +101,43 @@ function App() {
             <h2>Alku:</h2>
             <input onChange = {kellohändleralku} type="time" min="00:00" max="24:00" required></input>
             <h2>Loppu:</h2>
-            <input  onChange = {kellohändlerloppu} type="time" min="00:00" max="24:00" required></input>
+            <input onChange = {kellohändlerloppu} type="time" min="00:00" max="24:00" required></input>
+          </div>
           <h2>Fiilis:</h2>
-          <div className = "Hymynaamat">
-            <img src={hymy} alt="hymy"></img>
-            <img src={midi} alt="midi"></img>
-            <img src={suru} alt="suru"></img>
+          <div className = "Hymynaamat" id="Hymynaamat">
+            <img src={hymy} onClick={kuvahändler} alt="hymy" id='hymy'></img>
+            <img src={midi} onClick={kuvahändler} alt="midi" id="midi"></img>
+            <img src={suru} onClick={kuvahändler} alt="suru" id="suru"></img>
           </div>
+          <div className = "Tekstiboksi" id = "Tekstiboksi">
+            <h2>Muistiinpanot</h2>
+            <textarea onMouseLeave={muistiinpanohändler} className= "boksi"></textarea>
           </div>
+          <button className = "Lisäysnäppäin" onClick = {lisäämerkintäkalenteriin}>Lisää</button>
         </div>
         <div className = "Kalenteri"><h1>Kalenteri</h1></div>
         <div className = "Infopaneeli">
           <h1>
-          Infopaneeli
+          Info
           </h1>
+          <h2>
+            Tapahtuma: {nykyinenTapahtuma}
+          </h2>
+           <h2>
+            Laji: {nykyinenLaji}
+           </h2>
+           <h2>
+            kello alku: {nykyinenKelloAlku}
+           </h2>
+           <h2>
+            kello loppu: {nykyinenKelloLoppu}
+           </h2>
+           <h2>
+            naama: {nykyinenNaama}
+           </h2>
+           <h2>
+            muistiinpanot: {muistiinpanot}
+           </h2>
         </div>
         <div>
           <button className = "Lisää" onClick={lisäyshandler} style ={{visibility: painettu ? "visible" : "hidden"}}>
